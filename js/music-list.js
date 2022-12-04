c88cfa87433b302b97cb5d5f0247a1a4f72e5c7c
06776 <hajdarapatrik11@outlook.com>
2022-12-04 16:16:15 +0100

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,176 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Ritual",
+    artist: "Alan Walker",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "Angyal",
+    artist: "Burai X Missh X DISCO'S HIT",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "10:35",
+    artist: "Tiësto (feat. Tate McRae)",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Let It Rain Down",
+    artist: "Alle Farben (feat. PollyAnna)",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "Perfect Melody",
+    artist: "Jonas Blue, Julian Perretta",
+    img: "music-5",
+    src: "music-5",
+  },
+  {
+    name: "Vigyázz rám angyal",
+    artist: "Kozso, Ámokfutók",
+    img: "music-6",
+    src: "music-6",
+  },
+  {
+    name: "Bellissima",
+    artist: "Annalisa",
+    img: "music-7",
+    src: "music-7",
+  },
+  {
+    name: "Hold Me Closer",
+    artist: "Elton John, Britney Spears",
+    img: "music-8",
+    src: "music-8",
+  },
+  {
+    name: "Solatido",
+    artist: "Sugarloaf X Lotfi Begi",
+    img: "music-9",
+    src: "music-9",
+  },
+  {
+    name: "Moving On",
+    artist: "Dr. Alban X Admiral C4C X Lian Ross",
+    img: "music-10",
+    src: "music-10",
+  },
+  {
+    name: "When You're Gone",
+    artist: "Shawn Mendes",
+    img: "music-11",
+    src: "music-11",
+  },
+  {
+    name: "Headlights",
+    artist: "Alok & Alan Walker (feat. Kiddo)",
+    img: "music-12",
+    src: "music-12",
+  },
+  {
+    name: "Visszavágyom",
+    artist: "Caramel X Willcox",
+    img: "music-13",
+    src: "music-13",
+  },
+  {
+    name: "In The Dark",
+    artist: "Purple Disco Machine, Sophie and the Giants",
+    img: "music-14",
+    src: "music-14",
+  },
+  {
+    name: "Crush (Nem számítottam rád)",
+    artist: "Wellhello X Andro",
+    img: "music-15",
+    src: "music-15",
+  },
+  {
+    name: "Leave Before You Love Me",
+    artist: "Marshmello X Jonas Brothers",
+    img: "music-16",
+    src: "music-16",
+  },
+  {
+    name: "Paralyzed",
+    artist: "Big Time Rush",
+    img: "music-17",
+    src: "music-17",
+  },
+  {
+    name: "Million Dollar Baby",
+    artist: "Ava Max",
+    img: "music-18",
+    src: "music-18",
+  },
+  {
+    name: "I Believe",
+    artist: "Kamrad",
+    img: "music-19",
+    src: "music-19",
+  },
+  {
+    name: "Last Christmas",
+    artist: "Backstreet Boys",
+    img: "music-20",
+    src: "music-20",
+  },
+  {
+    name: "Újra",
+    artist: "Caramel",
+    img: "music-21",
+    src: "music-21",
+  },
+  {
+    name: "I'm Good (Blue)",
+    artist: "David Guetta & Bebe Rexha",
+    img: "music-22",
+    src: "music-22",
+  },
+  {
+    name: "Call It Love",
+    artist: "Felix Jaehn, Ray Dalton",
+    img: "music-23",
+    src: "music-23",
+  },
+  {
+    name: "Bad Memories",
+    artist: "Meduza, James Carter (ft. Elley Duhé, Fast Boy)",
+    img: "music-24",
+    src: "music-24",
+  },
+  {
+    name: "Lost Without You",
+    artist: "Kygo, Dean Lewis",
+    img: "music-25",
+    src: "music-25",
+  },
+  {
+    name: "Isole",
+    artist: "Tancredi",
+    img: "music-26",
+    src: "music-26",
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
